442caa1981ecda5e027b4f4ea28192cacce17bea
Tiara Feaster <tfeaster21@gmail.com>
2021-07-23 18:01:40 -0400

    added button functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("hey girl");
+  };
+
   return (
-    <div className="App">
-      <h2>colors now girlllll</h2>
+    <div>
+      <div className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(event) => setColor(event.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </div>
+      <div className="colors">
+        <h4>list goes here</h4>
+      </div>
     </div>
   );
 }
