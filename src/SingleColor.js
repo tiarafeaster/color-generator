fb972071b1ea7ea268eb6f6184898b28387cfc81
Tiara Feaster <tfeaster21@gmail.com>
2021-07-26 11:10:37 -0400

    added timeout for clipboard message to disappear after 3 seconds

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -7,6 +7,13 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
     <div
       className={`color ${index > 10 && "color-light"}`}
