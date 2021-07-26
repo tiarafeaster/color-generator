b7735555c8169de066f14a4fb9216ca2942ecf5c
Tiara Feaster <tfeaster21@gmail.com>
2021-07-26 11:04:16 -0400

    added functionality to copy hex to clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,11 +5,20 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const background = rgb.join(",");
   const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
 
   return (
-    <div className={`color`} style={{ backgroundColor: `rgb(${background})` }}>
+    <div
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${background})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </div>
   );
 };
